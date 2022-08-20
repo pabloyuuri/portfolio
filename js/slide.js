a3e1d8ee2ec22ff216f0bfcfe82ff3f7a9a89819
Pablo <pabloyuuri@gmail.com>
2022-08-19 23:29:14 -0300

    update cod

--- js/slide.js
+++ js/slide.js
@@ -2,9 +2,21 @@ class Slide
 {
   constructor(section) 
   {
+    this.toggleMenuRedesSociais();
     this.slide = document.querySelector(`[data-slide="${section}"]`);
     this.projectActive(0);
     this.init();
+  }
+
+  toggleMenu() {
+    console.log('nav');
+    const nav = document.querySelector('.nav-button');
+    nav.classList.toggle('active');
+  }
+  toggleMenuRedesSociais() {
+    let buttonHeaderName = document.querySelector('.header-button-name');
+    console.log('buttonHeaderName');
+    buttonHeaderName.addEventListener('click', this.toggleMenu)
   }
 
   projectActive(index) 
@@ -29,6 +41,30 @@ class Slide
     
   }
 
+  init()
+  {
+    this.next = this.next.bind(this);
+    this.back = this.back.bind(this);
+    this.info = this.info.bind(this);
+
+    this.projectBind();
+  }
+
+  projectBind()
+  {
+    const nextButton = this.getButton('.button-proximo');//document.querySelector('.button-proximo');
+    const backButton = this.getButton('.button-voltar');
+    let infosButtons = document.querySelectorAll('.button-informacao-projeto');
+
+    nextButton.addEventListener('click', this.next);
+    backButton.addEventListener('click', this.back);
+    infosButtons.forEach(item => item.addEventListener(['click'], this.info));
+  }
+  getButton(nameClass){
+    let nextButton = document.querySelector(nameClass);
+    return nextButton;
+  }
+  
   returnFirstProject(index)
   {
     this.listProjects[index].classList.add('active');
@@ -36,52 +72,40 @@ class Slide
 
   next() 
   {
+    this.removeInfo();
     this.projectActive(this.active + 1);
   }
 
   back() 
   {
+    this.removeInfo();
     this.projectActive(this.active - 1);
-  }
-
-  project()
-  {
-    const nextButton = document.querySelector('.button-proximo');
-    const backButton = document.querySelector('.button-voltar');
-    let infosButtons = document.querySelectorAll('.button-informacao-projeto');
-
-    nextButton.addEventListener('click', this.next);
-    backButton.addEventListener('click', this.back);
-    infosButtons.forEach(item => item.addEventListener(['click'], this.info));
-  }
-
-  init()
-  {
-
-    this.next = this.next.bind(this);
-    this.back = this.back.bind(this);
-    this.info = this.info.bind(this);
-
-    this.project();
   }
 
   info()
   {
-    console.log('chamou');
-    this.infos = document.querySelectorAll('.info');
-    
-    if(this.infoAtiva != "undefined" && this.infoAtiva != null)
-    {
-      this.infos[this.active].classList.remove('active');
-      this.infoAtiva = null;
+    if(this.infoAtiva != "undefined" && this.infoAtiva != null) {
+      this.removeInfo();
     }
-    else
-    {
-      this.infos[this.active].classList.add('active')
-      this.infoAtiva = this.infos[this.active];
+    else {
+      this.addInfo();
     }
   }
   
+  getInfos(){
+    let infos =  document.querySelectorAll('.info');
+    return infos;
+  }
+  addInfo(){
+    this.infos = this.getInfos();
+    this.infos[this.active].classList.add('active')
+    this.infoAtiva = this.infos[this.active];
+  }
+  removeInfo(){
+    this.infos = this.getInfos();
+    this.infos.forEach(item => item.classList.remove('active'));
+    this.infoAtiva = null;
+  }
 }
 
 new Slide('projetos');
